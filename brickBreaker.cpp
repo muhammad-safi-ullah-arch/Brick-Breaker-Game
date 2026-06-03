40e6ab84d2bafd47ac34f3f1d7ce896605d7e817
Muhammad Safi Ullah <miansafiullah786@gmail.com>
2026-06-03 16:13:33 +0500

    Interaction -- Faceoff mode Finalized

--- brickBreaker.cpp
+++ brickBreaker.cpp
@@ -1,139 +1,154 @@
 #include <SFML/Graphics.hpp>
 #include <optional>
-#include <cmath>
+#include <cstdlib>
+#include <ctime>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "2 Player Paddle Game");
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "Turn-Based Paddle Game");
     window.setFramerateLimit(60);
 
-    // PADDLE 1 (BOTTOM)
-    sf::RectangleShape paddle({100.f, 20.f});
-    paddle.setFillColor(sf::Color::Blue);
-    paddle.setOrigin({50.f, 10.f});
-    paddle.setPosition({400.f, 550.f});
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
 
-    // PADDLE 2 (TOP)
-    sf::RectangleShape paddle2({100.f, 20.f});
-    paddle2.setFillColor(sf::Color::Green);
-    paddle2.setOrigin({50.f, 10.f});
-    paddle2.setPosition({400.f, 50.f});
+    // PADDLES
+    sf::RectangleShape p1({100.f, 20.f});
+    p1.setOrigin({50.f, 10.f});
+    p1.setPosition({400.f, 550.f});
+    p1.setFillColor(sf::Color::Blue);
 
-    // BALL 1
+    sf::RectangleShape p2({100.f, 20.f});
+    p2.setOrigin({50.f, 10.f});
+    p2.setPosition({400.f, 50.f});
+    p2.setFillColor(sf::Color::Green);
+
+    // BALL
     sf::CircleShape ball(10.f);
     ball.setOrigin({10.f, 10.f});
-    ball.setPosition({400.f, 550.f});
-    sf::Vector2f vel{4.f, -4.f};
 
-    // BALL 2
-    sf::CircleShape ball2(10.f);
-    ball2.setOrigin({10.f, 10.f});
-    ball2.setPosition({400.f, 50.f});
-    sf::Vector2f vel2{4.f, 4.f};
-
-    // LIVES
-    int lives1 = 3;
-    int lives2 = 3;
-
-    // FONT (SFML 3 style)
+    // FONT
     sf::Font font("arial.ttf");
 
-    // TEXT
-    sf::Text t1(font, "Player 1 Lives: 3");
-    sf::Text t2(font, "Player 2 Lives: 3");
-
-    t1.setCharacterSize(20);
-    t2.setCharacterSize(20);
+    sf::Text t1(font, "", 20);
+    sf::Text t2(font, "", 20);
 
     t1.setPosition({10.f, 10.f});
-    t2.setPosition({520.f, 10.f});
+    t2.setPosition({600.f, 10.f});
+
+    int lives1 = 3, lives2 = 3;
+
+    // GAME STATE
+    int currentPlayer = (std::rand() % 2) + 1;
+    bool isMoving = true;
+    bool ballLaunched = false;
+    bool gameOver = false;
+
+    float paddleSpeed = 5.f;
+    float ballSpeed = 6.f;
+    sf::Vector2f ballVel(0, 0);
 
     while (window.isOpen()) {
 
         while (const std::optional event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>())
                 window.close();
-        }
 
-        // GAME OVER CHECK
-        bool gameOver = (lives1 == 0 || lives2 == 0);
+            if (!gameOver && event->is<sf::Event::KeyPressed>()) {
+                if (event->getIf<sf::Event::KeyPressed>()->code == sf::Keyboard::Key::Space) {
+
+                    if (!ballLaunched) {
+                        isMoving = false;
+                        ballLaunched = true;
+
+                        if (currentPlayer == 1)
+                            ballVel = {0, -ballSpeed};
+                        else
+                            ballVel = {0, ballSpeed};
+                    }
+                }
+            }
+        }
 
         if (!gameOver) {
 
-            // CONTROLS
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left) && paddle.getPosition().x > 50)
-                paddle.move({-7, 0});
-
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right) && paddle.getPosition().x < 750)
-                paddle.move({7, 0});
-
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A) && paddle2.getPosition().x > 50)
-                paddle2.move({-7, 0});
-
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D) && paddle2.getPosition().x < 750)
-                paddle2.move({7, 0});
-
-            // MOVE BALLS
-            ball.move(vel);
-            ball2.move(vel2);
-
-            // WALL COLLISION (BALL 1)
-            if (ball.getPosition().x < 10 || ball.getPosition().x > 790)
-                vel.x = -vel.x;
-
-            if (ball.getPosition().y < 10)
-                vel.y = -vel.y;
-
-            // PLAYER 1 LOSES LIFE
-            if (ball.getPosition().y > 600) {
-                if (lives1 > 0) lives1--;
-                ball.setPosition({400.f, 550.f});
+            // AUTO MOVE
+            if (isMoving) {
+                if (currentPlayer == 1) {
+                    p1.move({paddleSpeed, 0});
+                    if (p1.getPosition().x > 750 || p1.getPosition().x < 50)
+                        paddleSpeed = -paddleSpeed;
+                } else {
+                    p2.move({paddleSpeed, 0});
+                    if (p2.getPosition().x > 750 || p2.getPosition().x < 50)
+                        paddleSpeed = -paddleSpeed;
+                }
             }
 
-            // WALL COLLISION (BALL 2)
-            if (ball2.getPosition().x < 10 || ball2.getPosition().x > 790)
-                vel2.x = -vel2.x;
+            // BALL STICK
+            if (!ballLaunched) {
+                if (currentPlayer == 1)
+                    ball.setPosition({p1.getPosition().x, p1.getPosition().y - 20});
+                else
+                    ball.setPosition({p2.getPosition().x, p2.getPosition().y + 20});
+            }
 
-            if (ball2.getPosition().y > 590)
-                vel2.y = -vel2.y;
+            // MOVE BALL
+            if (ballLaunched)
+                ball.move(ballVel);
 
-            // PLAYER 2 LOSES LIFE
-            if (ball2.getPosition().y < 0) {
+            // HIT CHECK
+            if (currentPlayer == 1 &&
+                ball.getGlobalBounds().findIntersection(p2.getGlobalBounds())) {
+
                 if (lives2 > 0) lives2--;
-                ball2.setPosition({400.f, 50.f});
+
+                if (lives2 == 0) gameOver = true;
+
+                ballLaunched = false;
+                isMoving = true;
+
+                p2.setPosition({static_cast<float>(50 + rand() % 700), 50});
+            }
+            else if (currentPlayer == 2 &&
+                     ball.getGlobalBounds().findIntersection(p1.getGlobalBounds())) {
+
+                if (lives1 > 0) lives1--;
+
+                if (lives1 == 0) gameOver = true;
+
+                ballLaunched = false;
+                isMoving = true;
+
+                p1.setPosition({static_cast<float>(50 + rand() % 700), 550});
             }
 
-            // PADDLE COLLISION
-            if (ball.getGlobalBounds().findIntersection(paddle.getGlobalBounds()))
-                vel.y = -std::abs(vel.y);
+            // MISS
+            if (ball.getPosition().y < 0 || ball.getPosition().y > 600) {
+                ballLaunched = false;
+                isMoving = true;
 
-            if (ball2.getGlobalBounds().findIntersection(paddle2.getGlobalBounds()))
-                vel2.y = std::abs(vel2.y);
+                currentPlayer = (currentPlayer == 1) ? 2 : 1;
+            }
         }
 
         // TEXT UPDATE
-        if (!gameOver) {
-            t1.setString("Player 1 Lives: " + std::to_string(lives1));
-            t2.setString("Player 2 Lives: " + std::to_string(lives2));
-        } else {
+        t1.setString("P1 Lives: " + std::to_string(lives1));
+        t2.setString("P2 Lives: " + std::to_string(lives2));
+
+        if (gameOver) {
             if (lives1 == 0) {
-                t1.setString("Player 1 LOST!");
-                t2.setString("Player 2 WINS!");
+                t1.setString("P1 LOST");
+                t2.setString("P2 WINS");
             } else {
-                t2.setString("Player 2 LOST!");
-                t1.setString("Player 1 WINS!");
+                t2.setString("P2 LOST");
+                t1.setString("P1 WINS");
             }
         }
 
-        // DRAW
         window.clear();
-
-        window.draw(paddle);
-        window.draw(paddle2);
+        window.draw(p1);
+        window.draw(p2);
         window.draw(ball);
-        window.draw(ball2);
         window.draw(t1);
         window.draw(t2);
-
         window.display();
     }
 
